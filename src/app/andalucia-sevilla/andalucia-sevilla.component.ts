2928645cdd36acf9372b85ee4925ef63c299b79c
rferpae569 <rferpae569@g.educaand.es>
2024-04-10 20:55:31 +0200

    andaluciaSevilla Ruben Fernandez

--- src/app/andalucia-sevilla/andalucia-sevilla.component.ts
+++ src/app/andalucia-sevilla/andalucia-sevilla.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-andalucia-sevilla',
   templateUrl: './andalucia-sevilla.component.html',
-  styleUrls: ['./andalucia-sevilla.component.scss']
+  styleUrls: ['./andalucia-sevilla.component.scss'],
+  animations: [
+    trigger('slideDown', [
+      state('void', style({
+        transform: 'translateY(-100%)',
+        opacity: 0
+      })),
+      state('*', style({
+        transform: 'translateY(0)',
+        opacity: 1
+      })),
+      transition('void => *', animate('1000ms ease-out'))
+    ])
+  ]
 })
-export class AndaluciaSevillaComponent {
+export class AndaluciaSevillaComponent implements OnInit {
+  
+  images!: NodeListOf<HTMLImageElement>;
+  currentIndex: number = 0;
+  interval: number = 5000; // 5 segundos
+  mostrarParrafo: boolean = false;
 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.images = document.querySelectorAll('.imagenes-catedral img');
+    this.showImage(this.currentIndex);
+    setInterval(() => this.nextImage(), this.interval);
+
+    //NO FUNCIONA
+     // Simular un retraso más largo antes de mostrar el párrafo
+  setTimeout(() => {
+    this.mostrarParrafo = true;
+  }, 5000); // Cambié el tiempo a 5000ms (5 segundos)
+  }
+
+  showImage(index: number): void {
+    this.images.forEach((img, i) => {
+      if (i === index) {
+        img.style.display = 'block';
+      } else {
+        img.style.display = 'none';
+      }
+    });
+  }
+
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.showImage(this.currentIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AndaluciaSevillaComponent } from './andalucia-sevilla/andalucia-sevilla.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { AndaluciaSevillaComponent } from './andalucia-sevilla/andalucia-sevilla
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
