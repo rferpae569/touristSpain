280fe38b8e1ee1f37f9689c6790bb2170a51af04
rferpae569 <rferpae569@g.educaand.es>
2024-04-15 09:51:41 +0200

    andalucia-Sevilla-CatedralGiralda Ruben Fernandez

--- src/app/andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component.ts
+++ src/app/andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component.ts
@@ -1,33 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-andalucia-sevilla-catedral-giralda',
   templateUrl: './andalucia-sevilla-catedral-giralda.component.html',
   styleUrls: ['./andalucia-sevilla-catedral-giralda.component.scss'],
-  animations: [
-    trigger('slideDown', [
-      state('void', style({
-        transform: 'translateY(-100%)',
-        opacity: 0
-      })),
-      state('*', style({
-        transform: 'translateY(0)',
-        opacity: 1
-      })),
-      transition('void => *', animate('1000ms ease-out'))
-    ])
-  ]
 })
 export class AndaluciaSevillaCatedralGiraldaComponent implements OnInit {
-
   images!: NodeListOf<HTMLImageElement>;
   currentIndex: number = 0;
   interval: number = 5000; // 5 segundos
   mostrarParrafo: boolean = false;
 
+  constructor() {}
 
-  constructor() { }
+  //Funciones necesarias para que la imagenes se cambien cada x tiempo
 
   ngOnInit(): void {
     this.images = document.querySelectorAll('.imagenes-catedral img');
