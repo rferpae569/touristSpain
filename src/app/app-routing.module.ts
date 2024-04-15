5564899f0699c026f0f6bbd00d8d23d9c9ddefcb
rferpae569 <rferpae569@g.educaand.es>
2024-04-15 10:29:30 +0200

    Rutas Ruben Fernandez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AndaluciaSevillaCatedralGiraldaComponent } from './andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component';
+import { AndaluciaSevillaRealAlcazarComponent } from './andalucia-sevilla-real-alcazar/andalucia-sevilla-real-alcazar.component';
 
 const routes: Routes = [
   { path: '', component: AndaluciaSevillaCatedralGiraldaComponent },
+  { path: 'RealAlcazarSevilla', component: AndaluciaSevillaRealAlcazarComponent } 
 ];
 
 @NgModule({
