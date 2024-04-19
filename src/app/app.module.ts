a1daa05c3ddbede39b1a7ce341508949d880d317
rferpae569 <rferpae569@g.educaand.es>
2024-04-19 20:53:10 +0200

    andalucia-Sevilla-PlazaEspana Ruben Fernandez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AndaluciaSevillaCatedralGiraldaComponent } from './andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component';
 import { AndaluciaSevillaRealAlcazarComponent } from './andalucia-sevilla-real-alcazar/andalucia-sevilla-real-alcazar.component';
+import { AndaluciaSevillaPlazaEspanaComponent } from './andalucia-sevilla-plaza-espana/andalucia-sevilla-plaza-espana.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AndaluciaSevillaCatedralGiraldaComponent,
-    AndaluciaSevillaRealAlcazarComponent
+    AndaluciaSevillaRealAlcazarComponent,
+    AndaluciaSevillaPlazaEspanaComponent
   ],
   imports: [
     BrowserModule,
