caea91985b3a204b7d7d65d91b85c14efbd24f72
rferpae569 <rferpae569@g.educaand.es>
2024-04-20 14:35:38 +0200

    andalucia-Sevilla-CatedralGiralda Ruben Fernandez

--- src/app/andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component.ts
+++ src/app/andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component.ts
@@ -1,38 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-andalucia-sevilla-catedral-giralda',
   templateUrl: './andalucia-sevilla-catedral-giralda.component.html',
   styleUrls: ['./andalucia-sevilla-catedral-giralda.component.scss'],
 })
-export class AndaluciaSevillaCatedralGiraldaComponent implements OnInit {
-  images!: NodeListOf<HTMLImageElement>;
-  currentIndex: number = 0;
-  interval: number = 5000; // 5 segundos
-  mostrarParrafo: boolean = false;
+export class AndaluciaSevillaCatedralGiraldaComponent {
 
   constructor() {}
 
-  //Funciones necesarias para que la imagenes se cambien cada x tiempo
-
-  ngOnInit(): void {
-    this.images = document.querySelectorAll('.imagenes-catedral img');
-    this.showImage(this.currentIndex);
-    setInterval(() => this.nextImage(), this.interval);
-  }
-
-  showImage(index: number): void {
-    this.images.forEach((img, i) => {
-      if (i === index) {
-        img.style.display = 'block';
-      } else {
-        img.style.display = 'none';
-      }
-    });
-  }
-
-  nextImage(): void {
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    this.showImage(this.currentIndex);
-  }
+  
 }
