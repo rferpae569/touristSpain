cb6139a7aef001ccf939d7aaf2d2561fd15615cb
rferpae569 <rferpae569@g.educaand.es>
2024-04-26 21:17:22 +0200

    Rutas Ruben Fernandez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AndaluciaSevillaCatedralGiraldaComponent } from './andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component';
-// import { AndaluciaSevillaRealAlcazarComponent } from './andalucia-sevilla-real-alcazar/andalucia-sevilla-real-alcazar.component';
-// import { AndaluciaSevillaPlazaEspanaComponent } from './andalucia-sevilla-plaza-espana/andalucia-sevilla-plaza-espana.component';
 import { AndaluciaSevillaCatedralGiraldaComponent } from './Monumentos-Sevilla/andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component';
 import { AndaluciaSevillaRealAlcazarComponent } from './Monumentos-Sevilla/andalucia-sevilla-real-alcazar/andalucia-sevilla-real-alcazar.component';
 import { AndaluciaSevillaPlazaEspanaComponent } from './Monumentos-Sevilla/andalucia-sevilla-plaza-espana/andalucia-sevilla-plaza-espana.component';
