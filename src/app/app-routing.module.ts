b4e64f5d93dffcfcf28474785c41b6df33d11862
rferpae569 <rferpae569@g.educaand.es>
2024-04-27 17:13:40 +0200

    Rutas Ruben Fernandez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AndaluciaSevillaCatedralGiraldaComponent } from './Monumentos-Sevilla/a
 import { AndaluciaSevillaRealAlcazarComponent } from './Monumentos-Sevilla/andalucia-sevilla-real-alcazar/andalucia-sevilla-real-alcazar.component';
 import { AndaluciaSevillaPlazaEspanaComponent } from './Monumentos-Sevilla/andalucia-sevilla-plaza-espana/andalucia-sevilla-plaza-espana.component';
 import { AndaluciaSevillaMaestranzaComponent } from './Monumentos-Sevilla/andalucia-sevilla-maestranza/andalucia-sevilla-maestranza.component';
+import { AndaluciaSevillaTorreOroComponent } from './Monumentos-Sevilla/andalucia-sevilla-torre-oro/andalucia-sevilla-torre-oro.component';
 import { SevillaMenuComponent } from './sevilla-menu/sevilla-menu.component';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'CatedralGiraldaSevilla', component: AndaluciaSevillaCatedralGiraldaComponent },
   { path: 'RealAlcazarSevilla', component: AndaluciaSevillaRealAlcazarComponent } ,
   { path: 'PlazaEspanaSevilla', component: AndaluciaSevillaPlazaEspanaComponent },
-  { path: 'MaestranzaSevilla', component: AndaluciaSevillaMaestranzaComponent } 
+  { path: 'MaestranzaSevilla', component: AndaluciaSevillaMaestranzaComponent },
+  { path: 'TorreOroSevilla', component:AndaluciaSevillaTorreOroComponent} 
 ];
 
 @NgModule({
