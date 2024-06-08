fc832883f60970e11f7a5bf4f256635b42e407cb
rferpae569 <rferpae569@g.educaand.es>
2024-06-08 23:27:05 +0200

    MapaEspaña Ruben Fernandez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapaEspanaComponent } from './mapa-espana/mapa-espana.component';
 import { AndaluciaSevillaCatedralGiraldaComponent } from './Monumentos-Sevilla/andalucia-sevilla-catedral-giralda/andalucia-sevilla-catedral-giralda.component';
 import { AndaluciaSevillaRealAlcazarComponent } from './Monumentos-Sevilla/andalucia-sevilla-real-alcazar/andalucia-sevilla-real-alcazar.component';
 import { AndaluciaSevillaPlazaEspanaComponent } from './Monumentos-Sevilla/andalucia-sevilla-plaza-espana/andalucia-sevilla-plaza-espana.component';
@@ -23,8 +24,8 @@ import { AndaluciaSevillaAcuarioComponent } from './Monumentos-Sevilla/andalucia
 import { SevillaMenuComponent } from './sevilla-menu/sevilla-menu.component';
 
 const routes: Routes = [
-  {path: '', component:SevillaMenuComponent},
-  { path: 'CatedralGiraldaSevilla', component: AndaluciaSevillaCatedralGiraldaComponent },
+  { path: '', component:MapaEspanaComponent},
+  // { path: 'CatedralGiraldaSevilla', component: AndaluciaSevillaCatedralGiraldaComponent },
   { path: 'RealAlcazarSevilla', component: AndaluciaSevillaRealAlcazarComponent } ,
   { path: 'PlazaEspanaSevilla', component: AndaluciaSevillaPlazaEspanaComponent },
   { path: 'MaestranzaSevilla', component: AndaluciaSevillaMaestranzaComponent },
---
+++ src/app/mapa-espana/mapa-espana.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-espana',
+  templateUrl: './mapa-espana.component.html',
+  styleUrls: ['./mapa-espana.component.scss']
+})
+export class MapaEspanaComponent {
+
+}
