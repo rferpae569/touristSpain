a2fdc32241bd5d214565e3248851befa9091258c
rferpae569 <rferpae569@g.educaand.es>
2024-06-14 11:44:06 +0200

    Modulos Ruben Fernandez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import { AndaluciaSevillaPabellonNavegacionComponent } from './Monumentos-Sevill
 import { AndaluciaSevillaAcuarioComponent } from './Monumentos-Sevilla/andalucia-sevilla-acuario/andalucia-sevilla-acuario.component';
 import { MapaEspanaComponent } from './mapa-espana/mapa-espana.component';
 import { PoliticaprivacidadComponent } from './politicaprivacidad/politicaprivacidad.component';
-// import { AvisoLegalComponent } from './aviso-legal/aviso-legal.component';
+import { AvisoLegalComponent } from './aviso-legal/aviso-legal.component';
 
 @NgModule({
   declarations: [
