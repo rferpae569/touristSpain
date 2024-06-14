ccd64955bb35e532da0f070b58aba5e60b1f5d20
rferpae569 <rferpae569@g.educaand.es>
2024-06-14 11:58:54 +0200

    MapaEspaña Ruben Fernandez

--- src/app/mapa-espana/mapa-espana.component.ts
+++ src/app/mapa-espana/mapa-espana.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-mapa-espana',
   templateUrl: './mapa-espana.component.html',
-  styleUrls: ['./mapa-espana.component.scss']
+  styleUrls: ['./mapa-espana.component.scss'],
 })
 export class MapaEspanaComponent implements OnInit {
   showCookieConsent: boolean = true;
@@ -23,7 +23,9 @@ export class MapaEspanaComponent implements OnInit {
   }
 
   acceptCookies(): void {
-    this.cookieService.set('cookies', 'Aceptadas', 365);
+    const expirationDate = new Date();
+    expirationDate.setHours(expirationDate.getHours() + 24);
+    this.cookieService.set('cookies', 'Aceptadas', { expires: expirationDate });
     this.showCookieConsent = false;
   }
 
